f39c9938300a556431717e9929213cc3ec9043bd
pablo <pablo@pdebian>
2023-12-18 21:21:13 +0100

    add to src

--- src/speak.cpp
+++ src/speak.cpp
@@ -10,7 +10,7 @@ Speak::Speak(QObject *parent) : QObject(parent)
     m_speech->setVoice(m_voices.at(1));
     m_speech->setRate(0.1);
     m_speech->setPitch(0.5);
-    m_speech->setVolume(0.5);
+    m_speech->setVolume(1);
 }
 
 void Speak::talk(const QString &text)
